e15e8b4581e3bf867df2ec8e19cf803cc6284282
linzj <linzj@ucweb.com>
2014-07-11 18:10:16 +0800

    support android 4.4

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,14 @@
 #include "LightSnapshotHandler.h"
 
 typedef uint32_t uptr;
-
+typedef size_t (*MallocDebugMallocUsableSize)(void*);
 struct MallocDebug {
     void* (*malloc)(uptr bytes);
     void (*free)(void* mem);
     void* (*calloc)(uptr n_elements, uptr elem_size);
     void* (*realloc)(void* oldMem, uptr bytes);
     void* (*memalign)(uptr alignment, uptr bytes);
+    MallocDebugMallocUsableSize malloc_usable_size;
 };
 
 #define WRAP(x) _##x
@@ -29,14 +30,15 @@ static void _free(void* data);
 static void* _calloc(uptr n_elements, uptr elem_size);
 static void* _realloc(void* oldMem, uptr bytes);
 static void* _memalign(uptr alignment, uptr bytes);
+static size_t _malloc_usable_size(void*);
 const MallocDebug _malloc_dispatch __attribute__((aligned(32))) = {
-    WRAP(malloc), WRAP(free), WRAP(calloc), WRAP(realloc), WRAP(memalign)
+    WRAP(malloc), WRAP(free), WRAP(calloc), WRAP(realloc), WRAP(memalign), WRAP(malloc_usable_size)
 };
 #undef WRAP
 
 #define WRAP(x) dl##x
 const MallocDebug _dlmalloc_dispatch __attribute__((aligned(32))) = {
-    WRAP(malloc), WRAP(free), WRAP(calloc), WRAP(realloc), WRAP(memalign)
+    WRAP(malloc), WRAP(free), WRAP(calloc), WRAP(realloc), WRAP(memalign), WRAP(malloc_usable_size)
 };
 
 static void overrideMalloc()
@@ -124,6 +126,11 @@ static void* _memalign(uptr alignment, uptr bytes)
     }
     HeapInfo::unlockHeapInfo();
     return data;
+}
+
+static size_t _malloc_usable_size(void* ptr)
+{
+    return dlmalloc_usable_size(ptr);
 }
 
 class Constructor {
